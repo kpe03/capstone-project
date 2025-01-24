2a8d10d1c3be0138c67546b83f372b1a22cde023
izak <kazipriom@gmail.com>
2025-01-23 20:12:32 -0600

    First commit, and created checkPriorities function

--- testing/unitTest.cpp
+++ testing/unitTest.cpp
@@ -1,1 +1,22 @@
-/* Unit Test for C++*/
+/* Unit Test for C++
+Kazi Priom
+Ticket 2
+Date: 1/23/2024
+*/
+
+#include <iostream>
+#include <unordered_map>
+#include <string>
+#include <catch2/catch.hpp>
+
+int main()
+{
+}
+
+bool checkPriorities(std::string priorities[], std::unordered_map<int, std::string> values)
+{
+
+    for (int i = 0; i < priorities.size(); i++)
+    {
+    }
+};
