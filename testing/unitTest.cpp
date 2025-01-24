125fa7b6d272f2fbb2dd70caa50ba385599a4851
izak <kazipriom@gmail.com>
2025-01-23 22:15:25 -0600

    Finished checking priority order with if, else conditional

--- testing/unitTest.cpp
+++ testing/unitTest.cpp
@@ -9,6 +9,47 @@ Date: 1/23/2024
 #include <string>
 #include <cmath>
 // #include<catch2 / catch.hpp>
+class unitTest
+{
+
+public:
+    void checkPriorities(std::unordered_map<std::string, int> actual_metrics)
+    {
+
+        std::string priorities[] = {"temperature", "humidity", "energy"};
+        std::unordered_map<std::string, int> desired_metrics;
+        desired_metrics.insert(std::make_pair("temperature", 20));
+        desired_metrics.insert(std::make_pair("humidity", 35));
+        desired_metrics.insert(std::make_pair("energy", 70));
+
+        std::unordered_map<std::string, double> difference;
+
+        for (int i = 0; i < sizeof(priorities) / sizeof(priorities[0]); i++)
+        {
+            std::string current_property = priorities[i];
+            int actual_value = actual_metrics[current_property];
+            int desired_value = desired_metrics[current_property];
+
+            double diff = abs(actual_value - desired_value) / (double)desired_value * 100;
+            std::cout << diff << std::endl;
+            difference[current_property] = diff;
+        }
+
+        for (int i = 0; i < sizeof(priorities) / sizeof(priorities[1]) - 1; i++)
+        {
+            std::string key_high = priorities[i];
+            std::string key_low = priorities[i + 1];
+            if (difference[key_high] <= difference[key_low])
+            {
+                std::cout << "Priority order is incorrect: " << key_high << "should have a lower or equal difference than " << key_low;
+            }
+            else
+            {
+                std::cout << "Priority order is correct:" << key_high << "has a lower or equal difference than" << key_low;
+            }
+        }
+    };
+};
 
 int main()
 {
@@ -16,27 +57,8 @@ int main()
     mock_values.insert(std::make_pair("temperature", 21));
     mock_values.insert(std::make_pair("humidity", 40));
     mock_values.insert(std::make_pair("energy", 100));
+
+    unitTest test;
+
+    test.checkPriorities(mock_values);
 }
-
-void checkPriorities(std::unordered_map<std::string, int> actual_metrics)
-{
-
-    std::string priorities[] = {"temperature", "humidity", "energy"};
-    std::unordered_map<std::string, int> desired_metrics;
-    desired_metrics.insert(std::make_pair("temperature", 20));
-    desired_metrics.insert(std::make_pair("humidity", 35));
-    desired_metrics.insert(std::make_pair("energy", 70));
-
-    std::unordered_map<std::string, double> difference;
-
-    for (int i = 0; i < sizeof(priorities) / sizeof(priorities[0]); i++)
-    {
-        std::string current_property = priorities[i];
-        int actual_value = actual_metrics[current_property];
-        int desired_value = desired_metrics[current_property];
-
-        double diff = abs(actual_value - desired_value) / (double)desired_value * 100;
-        std::cout << diff;
-        difference[current_property] = diff;
-    }
-};
