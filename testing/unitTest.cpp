2e509c2639f1b71558b3392996ecc7e3c5792cfe
izak <kazipriom@gmail.com>
2025-01-23 21:49:08 -0600

    created mock values, and used function for compatibility with legacy versions of C++

--- testing/unitTest.cpp
+++ testing/unitTest.cpp
@@ -7,21 +7,36 @@ Date: 1/23/2024
 #include <iostream>
 #include <unordered_map>
 #include <string>
-#include <catch2/catch.hpp>
+#include <cmath>
+// #include<catch2 / catch.hpp>
 
 int main()
 {
+    std::unordered_map<std::string, int> mock_values;
+    mock_values.insert(std::make_pair("temperature", 21));
+    mock_values.insert(std::make_pair("humidity", 40));
+    mock_values.insert(std::make_pair("energy", 100));
 }
 
-void checkPriorities(std::unordered_map<int, std::string> actual_metrics)
+void checkPriorities(std::unordered_map<std::string, int> actual_metrics)
 {
 
-    std::string properties[] = {"temperature", "humidity", "energy"};
-    std::unordered_map desired_metrics = {{"temperature", 20},
-                                          {"humidity", 35},
-                                          {"energy", 70}};
+    std::string priorities[] = {"temperature", "humidity", "energy"};
+    std::unordered_map<std::string, int> desired_metrics;
+    desired_metrics.insert(std::make_pair("temperature", 20));
+    desired_metrics.insert(std::make_pair("humidity", 35));
+    desired_metrics.insert(std::make_pair("energy", 70));
 
-    for (int i = 0; i < priorities.size(); i++)
+    std::unordered_map<std::string, double> difference;
+
+    for (int i = 0; i < sizeof(priorities) / sizeof(priorities[0]); i++)
     {
+        std::string current_property = priorities[i];
+        int actual_value = actual_metrics[current_property];
+        int desired_value = desired_metrics[current_property];
+
+        double diff = abs(actual_value - desired_value) / (double)desired_value * 100;
+        std::cout << diff;
+        difference[current_property] = diff;
     }
 };
