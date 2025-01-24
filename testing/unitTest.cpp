dcc3f7b7c296c10ab363f69a925e242f3344015c
izak <kazipriom@gmail.com>
2025-01-23 20:59:40 -0600

    First commit, and created checkPriorities function

--- testing/unitTest.cpp
+++ testing/unitTest.cpp
@@ -13,8 +13,13 @@ int main()
 {
 }
 
-bool checkPriorities(std::string priorities[], std::unordered_map<int, std::string> values)
+void checkPriorities(std::unordered_map<int, std::string> actual_metrics)
 {
+
+    std::string properties[] = {"temperature", "humidity", "energy"};
+    std::unordered_map desired_metrics = {{"temperature", 20},
+                                          {"humidity", 35},
+                                          {"energy", 70}};
 
     for (int i = 0; i < priorities.size(); i++)
     {
