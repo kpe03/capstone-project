82419afeb145f0a33f618977901b10e976bb9e06
LandonLiles <98120313+LandonLiles@users.noreply.github.com>
2025-03-07 09:34:40 -0600

    integrated CO2 values into the signal sending algorithm

--- arduino_code/david.ino
+++ arduino_code/david.ino
@@ -2,9 +2,18 @@
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
+#include <Arduino.h>
+#include <Mhz19.h>
+#include <SoftwareSerial.h>
+
+SoftwareSerial softwareSerial(RX_PIN, TX_PIN); 
+Mhz19 sensor_co2;
 
 #define DHTPIN 7     // Digital pin connected to the DHT sensor
 #define DHTTYPE    DHT11
+
+#define RX_PIN 3     //RX pin
+#define TX_PIN 2     //TX pin
 
 DHT_Unified dht(DHTPIN, DHTTYPE);
 
@@ -14,6 +23,20 @@ DHT_Unified dht(DHTPIN, DHTTYPE);
 void setup() {
   Serial.begin(9600);
   dht.begin();
+
+  softwareSerial.begin(9600);
+
+  sensor_co2.begin(&softwareSerial);
+  sensor_co2.setMeasuringRange(Mhz19MeasuringRange::Ppm_5000);
+  sensor_co2.enableAutoBaseCalibration();
+
+  Serial.print("Preheating");  // Preheating, 3 minutes
+  while (!sensor_co2.isReady()) {
+    Serial.print(".");
+    delay(5000);
+  }
+
+  Serial.println("\nReady...");
 
   sensor_t sensor;
   dht.temperature().getSensor(&sensor);
@@ -29,14 +52,23 @@ void loop() {
   dht.humidity().getEvent(&event);
   float hum = event.relative_humidity;
 
+  auto CO2 = sensor_co2.getCarbonDioxide();
+  
+
   // if temp less than 70F send high signal to light
-  if (hum > 40) {
+  if(CO2 > 4000){ // CO2 takes highest priority
+    digitalWrite(outputPIN, HIGH);
+  }
+  else if (hum > 40) {
     digitalWrite(outputPIN, HIGH);
   }
   else {
     digitalWrite(outputPIN, LOW);
   }
 
+  if (CO2 >= 0) {
+    Serial.println("CO2: " + String(CO2) + " ppm");
+  }
   Serial.print("tem = "); Serial.println(temp,1);
   Serial.print("hum = "); Serial.println(hum,1);
 
