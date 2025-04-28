91156c003e13fd10049ebfdc7901d2c43d094b2f
LandonLiles <98120313+LandonLiles@users.noreply.github.com>
2025-04-28 18:03:56 -0500

    Delete arduino_code directory

--- arduino_code/Working_temp_hum_in_out_and_comfort/CheckComfortAlgorithm.h
+++
@@ -1,9 +0,0 @@
-#ifndef COMFORT_ALG_H
-#define COMFORT_ALG_H
-
-int checkHumComfortAlgorithm(int hum);
-int checkSmokeComfortAlgorithm(int smoke);
-int checkSmokeComfortAlgorithm(int smoke); 
-int checkCO2ComfortAlgorithm(int co2);
-
-#endif
